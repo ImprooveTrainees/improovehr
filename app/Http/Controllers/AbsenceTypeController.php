9ecffdcd7785a13ef4dbdaa3426f4009280a577b
diogopotes23 <61580536+diogopotes23@users.noreply.github.com>
2020-03-02 15:17:38 +0000

    tabelas_restantes_dp

---
+++ app/Http/Controllers/AbsenceTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\absenceType;
+use Illuminate\Http\Request;
+
+class AbsenceTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\absenceType  $absenceType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(absenceType $absenceType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\absenceType  $absenceType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(absenceType $absenceType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\absenceType  $absenceType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, absenceType $absenceType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\absenceType  $absenceType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(absenceType $absenceType)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_03_02_150749_create_absences_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->integer('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+
+            $table->integer('absence_type');
+            $table->foreign('absence_type')->references('id')->on('absence_types');
+
+            $table->string('attachment');
+            $table->string('status');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->string('motive');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absences');
+    }
+}
