e47e705a4acf0ef44f6b7c9583b6daa5e73e0ab4
André Surra <surra.andre@gmail.com>
2020-03-19 09:53:07 +0000

    register_new_user

--- public/js/myscripts.js
+++ public/js/myscripts.js
@@ -1,16 +1,31 @@
+// Employees Role
+
+$("#exampleRole").change(function(){
+    if($(this).val()=="other")
+    {
+        $("div#rolenew").css('display', 'grid');
+        // $("div#rolenew").show(); - Apenas para Display Block
+    }
+     else
+     {
+         $("div#rolenew").hide();
+     }
+ });
+
+
 // Add the following code if you want the name of the file appear on select
-$(".custom-file-input").on("change", function() {
+$(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-  });
+});
 
-  var icon = document.getElementById("imagefile");
+var icon = document.getElementById("imagefile");
 
-  icon.addEventListener('click', () => {
+icon.addEventListener('click', () => {
     $("input[type='file']").trigger('click');
-  })
+})
 
-  $('input[type="file"]').on('change', function() {
+$('input[type="file"]').on('change', function () {
     var val = $(this).val();
     $(this).siblings('span').text(val);
-  })
+})
