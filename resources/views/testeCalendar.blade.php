6e51f20978ee945f8cd97c6f8153e92638d0e435
André Lopes <51791359+Artix445@users.noreply.github.com>
2020-03-25 16:58:45 +0000

    Calendar: Function to select dates in calendar done

--- resources/views/testeCalendar.blade.php
+++ resources/views/testeCalendar.blade.php
@@ -2,32 +2,42 @@
 <head>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+<link href='assets/fullcalendar/core/main.css' rel='stylesheet' />
+<link href='assets/fullcalendar/daygrid/main.css' rel='stylesheet' />
 
-</head>
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+<script src='assets/fullcalendar/core/main.js'></script>
+<script src='assets/fullcalendar/daygrid/main.js'></script>
+<script src='assets/fullcalendar/interaction/main.js'></script>
 <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+  crossorigin="anonymous">
+</script>
+
+</head>
+
 
 
 <body>
     
-
     <h3>Calendar</h3>
     
     <div id='calendar'></div>
 
     <script>
-        $(document).ready(function() {
-            // page is now ready, initialize the calendar...
-            $('#calendar').fullCalendar({
-                // put your options and callbacks here
-                events : [
+        document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: [ 'interaction', 'dayGrid' ],
+            defaultView: 'dayGridMonth',
+            selectable: true,
+            height: 580,
+            select: function(info) {
+            alert('De ' + info.startStr + ' a ' + info.endStr);
+            var dataClicada =  info.date.getFullYear().toString() + (info.date.getMonth()+1).toString() + info.date.getDate().toString(); //aqui vai buscar a data clicada 
+            },
+          events : [
                     @foreach($events as $event)
                     {   
                         @if($event->Type == "Birthday")
@@ -48,15 +58,12 @@
                     },
                     @endforeach
                 ],
-                height: 580,
-                header: {
-                    left: '',
-                    center: 'title',
-                    right:  'today prev,next'
-                    
-                  }
-            })
+
         });
+
+        calendar.render();
+      });
+       
     </script>  
 
 
