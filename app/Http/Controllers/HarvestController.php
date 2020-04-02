63af21a04bbe7996b3ce1ff81128bf62e5612314
André Lopes <51791359+Artix445@users.noreply.github.com>
2020-04-02 16:28:31 +0100

    Harvest API - Hours and Time
    Harvest time and hours successfully retrieved from API. Hour per days of the week calculation done

--- app/Http/Controllers/HarvestController.php
+++ app/Http/Controllers/HarvestController.php
@@ -16,6 +16,8 @@ class HarvestController extends Controller
         //
         
         // header("Content-Type: application/json");
+
+        //Profile info harvest
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, 'https://api.harvestapp.com/v2/users/me');
@@ -37,9 +39,46 @@ class HarvestController extends Controller
 
       
         $result = json_decode($result); //transforma json em objecto
+        //End profile info Harvest
 
 
-        return view('testeHarvest')->with('msg', $result);
+        //Time entries Harvest
+        $ch2 = curl_init();
+
+        curl_setopt($ch2, CURLOPT_URL, 'https://api.harvestapp.com/v2/time_entries?user_id=3206639&');
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'GET');
+
+
+        $headers2 = array();
+        $headers2[] = 'Harvest-Account-Id: 1270741';
+        $headers2[] = 'Authorization: Bearer 2275709.pt.c6eIYJ4rw1djonReSiOhr9RfEdZCvvtVb_oBG0UaDhbaOx54c9dpzDrO9QgpG-SNuPutMguXIVx-b8UVE-tI9Q';
+        $headers2[] = 'User-Agent: ImprooveHR(andre.lopes@gmail.com)';
+        curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers2);
+
+        $result2 = curl_exec($ch2);
+        if (curl_errno($ch2)) {
+            echo 'Error:' . curl_error($ch2);
+        }
+        curl_close($ch2);
+
+        $result2 = json_decode($result2);
+
+
+        //end Time entries Harvest
+
+
+
+
+        $month = date('F');
+        $week = date( 'F d', strtotime( 'monday this week' ) )." | ". date( 'F d', strtotime( 'sunday this week' ) )." ".date('Y'); 
+        $monday = date( 'Y-m-d', strtotime( 'monday this week'));
+        $tuesday = date( 'Y-m-d', strtotime( 'tuesday this week'));
+        $wednesday = date( 'Y-m-d', strtotime( 'wednesday this week'));
+        $thursday = date( 'Y-m-d', strtotime( 'thursday this week'));
+        $friday = date( 'Y-m-d', strtotime( 'friday this week'));
+
+        return view('testeHarvest')->with('harvestProfile', $result)->with('harvestTimeInfo', $result2)->with('month', $month)->with('week', $week)->with('monday', $monday)->with('tuesday', $tuesday)->with('wednesday', $wednesday)->with('thursday', $thursday)->with('friday', $friday);
        
     }
 
--- resources/views/testeHarvest.blade.php
+++ resources/views/testeHarvest.blade.php
@@ -5,17 +5,122 @@
 </head>
 
 <body>
-<h2>Profile</h2>
-Name: <?php echo $msg->first_name ?> 
+
+<?php 
+echo $month;
+
+$mondayTotal = 0;
+$tuesdayTotal = 0;  
+$wednesdayTotal = 0; 
+$thursdayTotal = 0; 
+$fridayTotal = 0; 
+
+$totalHours = 0;
+
+
+for($i = 0; $i  < count($harvestTimeInfo->time_entries); $i++) {
+if($harvestTimeInfo->time_entries[$i]->spent_date == $monday) {
+    $mondayTotal += $harvestTimeInfo->time_entries[$i]->hours;
+    $totalHours += $harvestTimeInfo->time_entries[$i]->hours;
+
+}
+if($harvestTimeInfo->time_entries[$i]->spent_date == $tuesday) {
+    $tuesdayTotal += $harvestTimeInfo->time_entries[$i]->hours;
+    $totalHours += $harvestTimeInfo->time_entries[$i]->hours;
+
+}
+if($harvestTimeInfo->time_entries[$i]->spent_date == $wednesday) {
+    $wednesdayTotal += $harvestTimeInfo->time_entries[$i]->hours;
+    $totalHours += $harvestTimeInfo->time_entries[$i]->hours;
+
+}
+if($harvestTimeInfo->time_entries[$i]->spent_date == $thursday) {
+    $thursdayTotal += $harvestTimeInfo->time_entries[$i]->hours;
+    $totalHours += $harvestTimeInfo->time_entries[$i]->hours;
+
+}
+if($harvestTimeInfo->time_entries[$i]->spent_date == $friday) {
+    $fridayTotal += $harvestTimeInfo->time_entries[$i]->hours;
+    $totalHours += $harvestTimeInfo->time_entries[$i]->hours;
+
+}
+
+
+}
+
+
+?>
+
 <br>
-Last name: <?php echo $msg->last_name ?>
+<br>
+<br>
+<br>
+<img src="{{$harvestProfile->avatar_url}}">
+<br>
+{{$harvestProfile->first_name}}
+{{$harvestProfile->last_name}}
+<br>
+ {{$harvestProfile->email}} 
+
+ <br>
+ <br>
+
+ This Week
+ <br>
+<?php 
+echo $week;
+?>
+<br>
+{{$totalHours}} of 40 hours
+<br>
+<br>
+
+<table>
+<tr>
+
+<th>{{date( 'D', strtotime( 'monday this week'))}}
+    <br>
+    {{date( 'd F', strtotime( 'monday this week'))}}
+</th>
+<th>{{date( 'D', strtotime( 'tuesday this week'))}}
+    <br>
+    {{date( 'd F', strtotime( 'tuesday this week'))}}
+</th>
+<th>{{date( 'D', strtotime( 'wednesday this week'))}}
+    <br>
+    {{date( 'd F', strtotime( 'wednesday this week'))}}
+</th>
+<th>{{date( 'D', strtotime( 'thursday this week'))}}
+    <br>
+    {{date( 'd F', strtotime( 'thursday this week'))}}
+</th>
+<th>{{date( 'D', strtotime( 'friday this week'))}}
+    <br>
+    {{date( 'd F', strtotime( 'friday this week'))}}
+</th>
+
+</tr>
+
+
+
+<tr>
+<td>{{$mondayTotal}} hours</td>
+<td>{{$tuesdayTotal}} hours</td>
+<td>{{$wednesdayTotal}} hours</td>
+<td>{{$thursdayTotal}} hours</td>
+<td>{{$fridayTotal}} hours</td>
+</tr>
+
+
+
+
+
+
+</table>
 
 
 
 
 </body>
-
-
-
 
 </html>
