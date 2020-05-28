57eaf80a616f34947491fd3507b38c099c7d7e40
diogopotes23 <61580536+diogopotes23@users.noreply.github.com>
2020-05-28 14:49:26 +0100

    Update

--- app/Report.php
+++ app/Report.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
+
+    public function allAbsences() {
+
+
+        $allAbsences = DB::table('absences')
+        ->join('absence_types', 'absence_types.id', '=', 'absences.absencetype')
+        ->select('absences.*','absence_types.description as description')
+        ->get();
+
+        return $allAbsences;
+
+    }
+
 }
