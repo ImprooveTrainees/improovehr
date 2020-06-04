b7e98ef07491b20d7d88a5bbbb821cc85415d794
NunoViola <61583696+NunoViola@users.noreply.github.com>
2020-06-04 15:20:51 +0100

    add features holidays/absences page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,12 +52,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" id="btnlogin">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a id="linkfp" class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
